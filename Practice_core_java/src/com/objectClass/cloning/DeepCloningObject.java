522bd6f80efb8fc8352ced603b63307a9b811ff9
Rameshwar Singh <89194728+ramuatgnes@users.noreply.github.com>
2024-03-11 14:26:41 +0530

    commit

--- Practice_core_java/src/com/objectClass/cloning/DeepCloningObject.java
+++ Practice_core_java/src/com/objectClass/cloning/DeepCloningObject.java
@@ -2,11 +2,31 @@ package Practice_core_java.src.com.objectClass.cloning;
 
 public class DeepCloningObject implements Cloneable {
 
-    public  void  cloningTheObject(){
+    int a;
+
+    public static void  cloningTheObject() throws CloneNotSupportedException {
+
+        //creating object
+        DeepCloningObject object1 = new DeepCloningObject();
+
+        DeepCloningObject object2 = (DeepCloningObject) object1.clone();//Deep cloning
+
+        //initializing a with help of object1
+        object1.a = 10;
+        System.out.println("value of a: " + object1.a); //output=10
+
+        //initializing a with help of object2
+        object2.a = 20;
+        System.out.println("value of a: " + object2.a); //output=20
+        //printing object1 value
+        System.out.println("value of a: " + object1.a); //output=10
+
 
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws CloneNotSupportedException {
+
+        cloningTheObject();
 
     }
 }
