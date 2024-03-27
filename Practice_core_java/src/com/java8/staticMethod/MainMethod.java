6f84cfb934f6e0fb55df629d0cd4a5a00eb91b0d
Rameshwar Singh <89194728+ramuatgnes@users.noreply.github.com>
2024-03-27 12:37:37 +0530

    commit

---
+++ Practice_core_java/src/com/java8/staticMethod/MainMethod.java
@@ -0,0 +1,7 @@
+package Practice_core_java.src.com.java8.staticMethod;
+
+public interface MainMethod {
+    public static void main(String[] args) {
+        System.out.println("main method in interface ...");
+    }
+}
