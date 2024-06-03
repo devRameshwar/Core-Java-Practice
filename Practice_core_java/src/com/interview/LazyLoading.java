9351838736a7383fa732615415c036ec0754ea48
Rameshwar Singh <torameshwarsingh@gmail.com>
2024-06-03 11:22:38 +0530

    commit

---
+++ Practice_core_java/src/com/interview/LazyLoading.java
@@ -0,0 +1,23 @@
+package Practice_core_java.src.com.interview;
+
+public class LazyLoading {
+
+    private static LazyLoading instance;
+
+    public LazyLoading() {
+        System.out.println("Object is created...");
+    }
+
+    public static LazyLoading getLazyLoading() {
+        if (instance == null) {
+            instance = new LazyLoading();
+        }
+
+        System.out.println("Return the object....");
+        return instance;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(getLazyLoading());
+    }
+}
