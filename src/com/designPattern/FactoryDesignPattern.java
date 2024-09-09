1780fe403d91e0bdefda54a61d705ab1c7cf086e
Rameshwar Singh <torameshwarsingh@gmail.com>
2024-09-09 15:53:57 +0530

    Factory Design pattern

--- src/com/designPattern/FactoryDesignPattern.java
+++ src/com/designPattern/FactoryDesignPattern.java
@@ -4,6 +4,63 @@ public class FactoryDesignPattern {
 
 	public static void main(String[] args) {
 
+		MobileFactory factory = new MobileFactory(4);
+
+		Mobile mobile = factory.getMobile();
+
+		mobile.showMobile();
+	}
+
+}
+
+interface Mobile {
+
+	public void showMobile();
+}
+
+class Android implements Mobile {
+
+	@Override
+	public void showMobile() {
+		System.out.println("Android mobile show called..");
+	}
+
+}
+
+class IOS implements Mobile {
+
+	@Override
+	public void showMobile() {
+		System.out.println("IOS mobile show called...");
+	}
+
+}
+
+class Windows implements Mobile {
+
+	@Override
+	public void showMobile() {
+		System.out.println("Windows mobile show called..");
+	}
+
+}
+
+class MobileFactory {
+
+	Mobile mobile;
+
+	public MobileFactory(int id) {
+		if (id == 1) {
+			this.mobile = new Android();
+		} else if (id == 2) {
+			this.mobile = new Windows();
+		} else {
+			this.mobile = new IOS();
+		}
+	}
+
+	public Mobile getMobile() {
+		return mobile;
 	}
 
 }
